894d78cd6dde06912571c107eb7526378e1c20e2
ktrillos2 <ktrillos2@gmail.com>
2026-04-20 09:48:35 -0500

    Cambios para despliegue

--- app/gracias/page.tsx
+++ app/gracias/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { client } from "@/sanity/lib/client"
 import { THANK_YOU_PAGE_QUERY, GLOBAL_CONFIG_QUERY } from "@/sanity/lib/queries"
 import ClientGraciasPage from "./ClientGraciasPage"
@@ -6,5 +7,9 @@ export default async function GraciasPage() {
   const data = await client.fetch(THANK_YOU_PAGE_QUERY)
   const globalConfig = await client.fetch(GLOBAL_CONFIG_QUERY)
 
-  return <ClientGraciasPage data={data} globalConfig={globalConfig} />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ClientGraciasPage data={data} globalConfig={globalConfig} />
+    </Suspense>
+  )
 }
